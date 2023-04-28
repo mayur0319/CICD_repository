a05e571387d67de79317c172315ba55daf386b6d
mayursabade <mayur_sabade@persistent.com>
2023-04-28 17:40:25 +0530

    add codepipeline role in codebuild pipeline iam

--- iam-role.tf
+++ iam-role.tf
@@ -74,7 +74,8 @@ data "aws_iam_policy_document" "cicd-build-policies" {
                "s3:*",
                "codebuild:*",
                "secretsmanager:*",
-               "iam:*"
+               "iam:*",
+               "codepipeline:*"
                ]
     resources = ["*"]
     effect    = "Allow"
