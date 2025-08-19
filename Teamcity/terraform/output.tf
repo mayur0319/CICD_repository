54066b6f700bc803468ac63f140799e01793f5fb
mayur0319 <sabademayur@gmail.com>
2025-08-19 18:40:29 +0530

    Update TeamCity README with Terraform installation instructions and initialization steps; comment out output variables in Terraform configuration

--- Teamcity/terraform/output.tf
+++ Teamcity/terraform/output.tf
@@ -1,9 +1,9 @@
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.demo_bucket.bucket
-}
+# output "bucket_name" {
+#   description = "Name of the created S3 bucket"
+#   value       = aws_s3_bucket.demo_bucket.bucket
+# }
 
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.demo_bucket.arn
-}
+# output "bucket_arn" {
+#   description = "ARN of the created S3 bucket"
+#   value       = aws_s3_bucket.demo_bucket.arn
+# }
