72a4ddeb0f8789c338f5a4bfb8f06d99ee5a46a9
mayur0319 <sabademayur@gmail.com>
2025-08-21 20:38:29 +0530

    Remove S3 bucket ACL resource and retain public access block configuration

--- Teamcity/terraform/s3.tf
+++ Teamcity/terraform/s3.tf
@@ -8,11 +8,7 @@ resource "aws_s3_bucket" "bucket" {
   }
   force_destroy = true
 }
-# create bucket ACL :
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
+
 # block public access :
 resource "aws_s3_bucket_public_access_block" "public_block" {
   bucket                  = aws_s3_bucket.bucket.id
